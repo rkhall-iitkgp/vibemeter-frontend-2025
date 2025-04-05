7d93b9ba3eba5786ea9dfb48bfb68bd777a4fa46
code-Harsh247 <justharsh2407@gmail.com>
2025-04-06 05:03:02 +0530

    added useChat hook and webSocketActions

---
+++ src/hooks/useChat.ts
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import {
+  connectWebSocket,
+  disconnectWebSocket,
+  sendMessage,
+  stopBotResponse,
+} from "../store/actions/webSocketActions";
+
+import { RootState } from "../store";
+
+export const useChat = () => {
+  const dispatch = useDispatch();
+  const [inputMessage, setInputMessage] = useState<string>("");
+
+  const messages = useSelector((state: RootState) => state.chat.messages);
+  const connectionStatus = useSelector(
+    (state: RootState) => state.chat.connectionStatus,
+  );
+  const isTyping = useSelector((state: RootState) => state.chat.isBotTyping);
+  const isThinking = useSelector(
+    (state: RootState) => state.chat.isBotThinking,
+  );
+  const error = useSelector((state: RootState) => state.chat.error);
+
+  // Connect to WebSocket when hook is first used
+  useEffect(() => {
+    dispatch(connectWebSocket());
+
+    return () => {
+      dispatch(disconnectWebSocket());
+    };
+  }, [dispatch]);
+
+  const sendUserMessage = (message?: string) => {
+    if (inputMessage.trim() || message?.trim()) {
+      setInputMessage("");
+      dispatch(sendMessage(message || inputMessage));
+    }
+  };
+
+  const stopResponse = () => {
+    dispatch(stopBotResponse());
+  };
+
+  const reconnect = () => {
+    dispatch(connectWebSocket());
+  };
+
+  return {
+    messages,
+    inputMessage,
+    setInputMessage,
+    sendUserMessage,
+    connectionStatus,
+    reconnect,
+    stopResponse,
+    isTyping,
+    isThinking,
+    error,
+  };
+};
