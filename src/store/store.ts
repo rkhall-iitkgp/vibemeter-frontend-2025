47627bf71e2abbb1e977c6809954ccbc43decfea
code-Harsh247 <justharsh2407@gmail.com>
2025-04-06 05:22:27 +0530

    added chatSlice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import chatReducer from "./chatSlice";
 import authReducer from "./authSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    chat: chatReducer,
   },
 });
 
